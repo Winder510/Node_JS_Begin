1468ed74d3e6dd1c14d93e2a5b00077a4bc54bf7
Winder510 <hoangphonghp04@gmail.com>
2023-11-15 23:54:27 +0700

    EJS loop & cnt db

---
+++ src/configs/connectDB.js
@@ -0,0 +1,23 @@
+// get the client
+import { router } from 'json-server';
+import mysql from 'mysql2';
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'nodejsbasic'
+});
+
+// simple query
+// connection.query(
+//     'SELECT * FROM `user` ',
+//     function (err, results, fields) {
+//         let rows = results.map((row) => {
+//             return row;
+//         })
+//         console.log(rows);
+
+//     }
+// );
+export default connection;
--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,7 +1,18 @@
-
+import connection from "../configs/connectDB";
 
 let getHomePage = (req, res) => {
-    return res.render('index.ejs');
+    // logic
+    let data = [];
+    connection.query(
+        'SELECT * FROM `user` ',
+        function (err, results, fields) {
+            let data = results.map((row) => {
+                return row;
+            })
+            return res.render('index.ejs', { dataUser: data, test: 'abc string test' }); // truyen mot bien object sang file index.ejs
+        }
+    );
+
 }
 
 module.exports = {
